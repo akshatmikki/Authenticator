7cdf76f68d9744a371da45cfef7f003414c3dc8a
akshatmikki <71835769+akshatmikki@users.noreply.github.com>
2025-04-03 16:47:46 +0530

    Update page.tsx

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -24,7 +24,7 @@ export default function Posts() {
         });
 
         if (res.status === 200 && res.data?.data?.length) {
-          setPosts(res.data.data);
+          setPosts(res.data);
         } else {
           setError("No posts available.");
         }
